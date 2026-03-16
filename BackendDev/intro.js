2a462dee6d56eeaafce39f1735648d03f288a7b0
Temidayo Emmanuel Fagbemi <82624396+zordtech@users.noreply.github.com>
2026-03-16 14:53:11 +0200

    Add files via upload

---
+++ BackendDev/intro.js
@@ -0,0 +1,9 @@
+const person = {
+  name: "Temidayo Emmanuel",
+  age: 24,
+  favoriteLanguage: "C++"
+};
+
+console.log(`Name: ${person.name}`);
+console.log(`Age: ${person.age}`);
+console.log(`Favorite Programming Language: ${person.favoriteLanguage}`);
